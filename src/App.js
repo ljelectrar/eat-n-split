2cfede5959e89c1ded18e36b0cab129fd0256c4a
ljelectrar <ljelectrar@gmail.com>
2024-03-28 20:42:55 -0300

    Some dynamic added...

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
     name: "Matheus",
-    image: "https://media.licdn.com/dms/image/C4E03AQHipfecLDt15g/profile-displayphoto-shrink_800_800/0/1614798966051?e=1717027200&v=beta&t=kT6e05qAW8FF627EwviHzkOqkJd0x-xhtF6zEC-ojbY",
+    image: "https://i.pravatar.cc/48?$=1",
     balance: -7,
   },
   {
     id: 933372,
     name: "Júlia",
-    image: "https://media.licdn.com/dms/image/D4D35AQEIM5PRMqU6ew/profile-framedphoto-shrink_800_800/0/1701185896637?e=1712242800&v=beta&t=ZJVjv9yxEel0P0wF57fqWCvojCGeZyBZ03Deyn8mDnA",
+    image: "https://i.pravatar.cc/49?=$1",
     balance: 20,
   },
   {
     id: 499476,
     name: "Arthur Juan",
-    image: "https://media.licdn.com/dms/image/D4E35AQHhMo2muNERsg/profile-framedphoto-shrink_800_800/0/1653270464211?e=1712242800&v=beta&t=Oj4Wims-47hovm4GijuSJRlr0pt7ihJAfa_6yTKWUaA",
+    image: "https://i.pravatar.cc/48?=$0",
     balance: 0,
   },
 ];
 
+function Button({children, onClick}) {
+  return <button className="button" onClick={onClick}>{children}</button>
+}
 
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(true);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
   return <div className="app">
     <div className="sidebar">
       <FriendList />
-      <FormAddFriend />
-      <Button>Add Friend</Button>
+      {showAddFriend && <FormAddFriend />}
+      <Button onClick={handleShowAddFriend} >{showAddFriend ? "Close" : "Add Friend"}</Button>
     </div>
     <FormSplitBill />
   </div>
@@ -56,16 +67,39 @@ function Friend({friend}){
     </li>
 }
 
-function Button({children}) {
-  return <button className="button">{children}</button>
-}
-
 function FormAddFriend() {
-  return <form className="form-add-friend">
+
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if(!name && !image) {
+      return;
+    }
+
+    const id = crypto.randomUUID;
+
+    const newFriend = {
+      id,
+      name, 
+      image: `${image}?=${id}`, 
+      balance: 0,
+    };
+
+    console.log(newFriend);
+
+    setName("");
+    setImage("https://i.pravatar.cc/48");
+  }
+
+  return <form className="form-add-friend" onSubmit={handleSubmit}>
     <label>Friend name 😎</label>
-    <input type="text" input/>
+    <input type="text" value={name} onChange={e => setName(e.target.value)}/>
+
     <label>Image URL 🖼️</label>
-    <input type="text" input/>
+    <input type="text" value={image} onChange={e => setImage(e.target.value)}/>
     <Button>Add</Button>
   </form>
 }
